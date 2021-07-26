7f7ddda313ddf5cd03ca698d072f5c59532b5739
luispozas <luis.pozaspalomo@gmail.com>
2021-07-26 08:55:32 +0200

    m

---
+++ Saludar.java
@@ -0,0 +1,8 @@
+public class Saludar { 
+
+    public static void main(String args[]) { 
+        String name = args[0];
+        name = name.toUpperCase();
+        System.out.println(name); 
+    }
+}
